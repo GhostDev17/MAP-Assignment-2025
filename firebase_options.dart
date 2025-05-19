2f59992dc90f8f939424f8b6d462430f7e755b50
GhostDev17 <pkmagongo@gmail.com>
2025-05-19 03:07:57 +0200

    Firebase

---
+++ firebase_options.dart
@@ -0,0 +1,16 @@
+// lib/firebase_options.dart
+
+import 'package:firebase_core/firebase_core.dart';
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    return const FirebaseOptions(
+      apiKey: "AIzaSyC5bEsUVNKjFwaeWM3feXjwMXF2X0v0R_A",
+      authDomain: "namibiahockey-77c0d.firebaseapp.com",
+      projectId: "namibiahockey-77c0d",
+      storageBucket: "namibiahockey-77c0d.firebasestorage.app",
+      messagingSenderId: "380353264677",
+      appId: "1:380353264677:web:378c717372ac50e0d7d3cc",
+    );
+  }
+}
